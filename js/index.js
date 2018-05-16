6bd7b006800cf997cef138193939398b4d35973c
Constantin Ehrensberger <ehrensberger.c@gmail.com>
2018-05-16 19:59:04 +0300

    modify matrix works now

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 var Game = {};
 
 var matrix = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
-    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
-    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 0, 0, 2, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 2, 0, 0, 0],
-    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 // Elements : 
@@ -33,49 +33,46 @@ var matrix = [
 
 
 Game.start = function () {
-    Game.display();
-    
+  Game.display();
+
 }
 
-Game.display = function() {
-    for (var i=0; i<matrix.length; i++) {
-        for (var j=0; j<matrix[i].length; j++) {
-            if (matrix[i][j] == 1) {
-                var ground = document.createElement('div');
-                $('#container').append($(ground));
-                $(ground).addClass('ground');
-            }
-            else if (matrix[i][j] == 2) {
-                var trunk = document.createElement('div');
-                $('#container').append($(trunk));
-                $(trunk).addClass('trunk');
-            }
-            else if (matrix[i][j] == 3) {
-                var leaf = document.createElement('div');
-                $('#container').append($(leaf));
-                $(leaf).addClass('leaf');
-            }
-            else if (matrix[i][j] == 4) {
-                var rock = document.createElement('div');
-                $('#container').append($(rock));
-                $(rock).addClass('rock');
-            }
-            else if (matrix[i][j] == 5) {
-                var grass = document.createElement('div');
-                $('#container').append($(grass));
-                $(grass).addClass('grass');
-            }
-            else if (matrix[i][j] == 6) {
-                var cloud = document.createElement('div');
-                $('#container').append($(cloud));
-                $(cloud).addClass('cloud');
-            }
-        }
+Game.display = function () {
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == 1) {
+        var ground = document.createElement('div');
+        $('#container').append($(ground));
+        $(ground).addClass('ground');
+      }
+      else if (matrix[i][j] == 2) {
+        var trunk = document.createElement('div');
+        $('#container').append($(trunk));
+        $(trunk).addClass('trunk');
+      }
+      else if (matrix[i][j] == 3) {
+        var leaf = document.createElement('div');
+        $('#container').append($(leaf));
+        $(leaf).addClass('leaf');
+      }
+      else if (matrix[i][j] == 4) {
+        var rock = document.createElement('div');
+        $('#container').append($(rock));
+        $(rock).addClass('rock');
+      }
+      else if (matrix[i][j] == 5) {
+        var grass = document.createElement('div');
+        $('#container').append($(grass));
+        $(grass).addClass('grass');
+      }
+      else if (matrix[i][j] == 6) {
+        var cloud = document.createElement('div');
+        $('#container').append($(cloud));
+        $(cloud).addClass('cloud');
+      }
     }
+  }
 }
-
-
-
 
 // ORIGINAL VERSION
 
@@ -97,7 +94,10 @@ Minecraft.matrix = []
 for (let row = 0; row < 10; row++) {
   Minecraft.matrix[row] = []
   for (let column = 0; column < 10; column++) {
-    Minecraft.matrix[row][column] = Minecraft.createDiv(row, column)
+    Minecraft.matrix[row][column] = Minecraft.createDiv(row, column);
+    if (row === 7 || row === 8 || row === 9){
+      Minecraft.matrix[row][column].addClass("dirt");
+    }
   }
 }
 
@@ -110,31 +110,47 @@ $('.box').click(Minecraft.clickFunc)
 
 // Matrix Modification
 
-Minecraft.userHolds = "dirt-inventory";
+Minecraft.userHolds = "";
+
+$('#menu-container button').click(function (e) {
+  Minecraft.userHolds = e.target.id
+});
 
 function modifyMatrix(e) {
-  let currentClasses = Minecraft.clickedBox.className
-  // only if the box is currently empty:
-  switch (Minecraft.userHolds) {
-    case "stone-inventory":
-      Minecraft.clickedBox.className = currentClasses + " stone";
-      break;
-    case "dirt-inventory":
-      Minecraft.clickedBox.className = currentClasses + " dirt";
-      break;
-    case "grass-inventory":
-      Minecraft.clickedBox.className = currentClasses + " grass";
-      break;
-    case "wood-inventory":
-      Minecraft.clickedBox.className = currentClasses + " wood";
-      break;
-    case "leaves-inventory":
-      Minecraft.clickedBox.className = currentClasses + " leaves";
-      break;
-    case "shovel":
-      Minecraft.clickedBox.className 
-    default:
-      Minecraft.clickedBox.className = currentClasses;
+  let boxIsEmpty = false;
+  Minecraft.clickedBox.classList.forEach(element => {
+    boxIsEmpty = (element !== "stone" && element !== "dirt" && element !== "grass" && element !== "wood" && element !== "leaves");
+  });
+  if (boxIsEmpty) {
+    switch (Minecraft.userHolds) {
+      case "stone-inventory":
+        Minecraft.clickedBox.className += " stone";
+        break;
+      case "dirt-inventory":
+        Minecraft.clickedBox.className += " dirt";
+        break;
+      case "grass-inventory":
+        Minecraft.clickedBox.className += " grass";
+        break;
+      case "wood-inventory":
+        Minecraft.clickedBox.className += " wood";
+        break;
+      case "leaves-inventory":
+        Minecraft.clickedBox.className += " leaves";
+        break;
+    }
   }
-
+  switch (Minecraft.userHolds) {
+    case "shovel":
+      Minecraft.clickedBox.className = Minecraft.clickedBox.className.split(" dirt")[0];
+      Minecraft.clickedBox.className = Minecraft.clickedBox.className.split(" grass")[0];
+      break;
+    case "pickaxe":
+      Minecraft.clickedBox.className = Minecraft.clickedBox.className.split(" stone")[0];
+      break;
+    case "axe":
+      Minecraft.clickedBox.className = Minecraft.clickedBox.className.split(" wood")[0];
+      Minecraft.clickedBox.className = Minecraft.clickedBox.className.split(" leaves")[0];
+      break;
+  }
 }
